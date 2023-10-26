9797964763aa5ed5432700af5f7e3ede9603d01b
Alex Mazurok | Мазурок Олександр | Zyabrik10 <78109707+Zyabrik10@users.noreply.github.com>
2023-10-26 20:32:45 +0200

    Routes
    Created routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+
+import { Signin, Signup, Start } from '../pages/index';
+
 export const App = () => {
-  return (<>Hello World!</>);
+  return (
+    <Routes>
+      <Route path="/" element={<Start />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+    </Routes>
+  );
 };
---
+++ src/pages/signin/Signin.jsx
@@ -0,0 +1,3 @@
+export const Signin = () => {
+    return <>Signin</>
+}
