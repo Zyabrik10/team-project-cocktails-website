d6bbad912f03de92d3b191ae9807706c9e3c425d
Olga16119 <tkachenko93@ukr.net>
2023-10-27 15:02:47 +0200

    Update SecMyDrinks.jsx

--- src/pages/my-drinks/sec-my-drinks/SecMyDrinks.jsx
+++ src/pages/my-drinks/sec-my-drinks/SecMyDrinks.jsx
@@ -1,18 +1,4 @@
-import React from 'react';
 
 export const SecMyDrinks = () => {
-  const drinks = [];
 
-  return (
-    <div>
-      {drinks.map((drink, index) => (
-        <div key={index}>
-            <img src={drink} alt={drink} />
-              <h3>{drink}</h3>
-              <p>{drink}</p>
-              <p>{ drink}</p>
-        </div>
-      ))}
-    </div>
-  );
 };
