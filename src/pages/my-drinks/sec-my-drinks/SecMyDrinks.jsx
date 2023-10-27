4ab84a6697bf2d5bc002ed8199c53e5f9bea209b
Olga16119 <tkachenko93@ukr.net>
2023-10-27 13:52:29 +0200

    add colours root

--- src/pages/my-drinks/sec-my-drinks/SecMyDrinks.jsx
+++ src/pages/my-drinks/sec-my-drinks/SecMyDrinks.jsx
@@ -1,1 +1,18 @@
-export const SecMyDrinks = () => {}
+import React from 'react';
+
+export const SecMyDrinks = () => {
+  const drinks = [];
+
+  return (
+    <div>
+      {drinks.map((drink, index) => (
+        <div key={index}>
+            <img src={drink} alt={drink} />
+              <h3>{drink}</h3>
+              <p>{drink}</p>
+              <p>{ drink}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
