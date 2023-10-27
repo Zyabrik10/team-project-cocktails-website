4a6f9f0deb07f19d2a829252065a47cf6816e78b
Alex Mazurok | Мазурок Олександр | Zyabrik10 <78109707+Zyabrik10@users.noreply.github.com>
2023-10-28 00:01:33 +0200

    Fix zIndex issue

--- src/layout/StartPages/StartPages.jsx
+++ src/layout/StartPages/StartPages.jsx
@@ -4,11 +4,13 @@ import css from './StartPages.module.css';
 
 export const StartPages = () => {
   return (
-    <div className={css["layout"]}>
-      <div className={css["des-1"]}></div>
-      <div className={css["des-2"]}></div>
-      <div className={css["des-3"]}></div>
-      <Outlet />
+    <div className={css['layout']}>
+      <div className={css['des-1']}></div>
+      <div className={css['des-2']}></div>
+      <div className={css['des-3']}></div>
+      <div style={{ position: 'relative', zIndex: '100' }}>
+        <Outlet />
+      </div>
     </div>
   );
 };
