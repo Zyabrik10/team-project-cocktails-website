7b257667e3c69152d349155a5400cb1830147209
Misha <shevchenkom606@gmail.com>
2023-10-28 15:03:06 +0200

    додав медіа 768

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,54 +12,60 @@ import LogoSvg from 'components/Header/components/Svg/LogoSvg';
 export const Footer = () => {
     return (
         <footer className={css.footer}>
-            <div className={css.box1}>
-                <div className={css.socialBox}>
-                    <NavLink to="/home" className={`${css["logoLink"]} ${globalCss["global-link"]}`}>
-                        <LogoSvg/>
-                        <p className={`${css["title"]} ${globalCss["global-p"]}`}>Drink Master</p>
-                    </NavLink>
-                    <ul className={`${css["socialList"]} ${globalCss["global-list"]}`}>
-                        <li className={css.socialItem}>
-                            <a className={`${css["socialLink"]} ${globalCss["global-link"]}`} href="https://www.facebook.com/goITclub/"><FacebookSvg/></a>
-                        </li>
-                        <li className={css.socialItem}>
-                            <a className={`${css["socialLink"]} ${globalCss["global-link"]}`} href="https://www.instagram.com/goitclub/"><InstagramSvg/></a>
-                        </li>
-                        <li className={css.socialItem}>
-                            <a className={`${css["socialLink"]} ${globalCss["global-link"]}`} href="https://www.youtube.com/c/GoIT"><YouTubeSvg/></a>
-                        </li>
-                    </ul>
+            <div className={css.circle1}></div>
+            <div className={css.circle2}></div>
+
+            <div className={css.footerMainBox}>
+                <div className={css.box1}>
+                    <div className={css.socialBox}>
+                        <NavLink to="/home" className={`${css["logoLink"]} ${globalCss["global-link"]}`}>
+                            <LogoSvg/>
+                            <p className={`${css["title"]} ${globalCss["global-p"]}`}>Drink Master</p>
+                        </NavLink>
+                        <ul className={`${css["socialList"]} ${globalCss["global-list"]}`}>
+                            <li className={css.socialItem}>
+                                <a className={`${css["socialLink"]} ${globalCss["global-link"]}`} href="https://www.facebook.com/goITclub/"><FacebookSvg/></a>
+                            </li>
+                            <li className={css.socialItem}>
+                                <a className={`${css["socialLink"]} ${globalCss["global-link"]}`} href="https://www.instagram.com/goitclub/"><InstagramSvg/></a>
+                            </li>
+                            <li className={css.socialItem}>
+                                <a className={`${css["socialLink"]} ${globalCss["global-link"]}`} href="https://www.youtube.com/c/GoIT"><YouTubeSvg/></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <nav className={css.nav}>
+                        <NavLink className={`${css["navLink"]} ${globalCss["global-link"]}`}>Drinks</NavLink>
+                        <NavLink className={`${css["navLink"]} ${globalCss["global-link"]}`}>Add drink</NavLink>
+                        <NavLink className={`${css["navLink"]} ${globalCss["global-link"]}`}>My drinks</NavLink>
+                        <NavLink className={`${css["navLink"]} ${globalCss["global-link"]}`}>Favorites drinks</NavLink>
+                    </nav>
                 </div>
-                <nav className={css.nav}>
-                    <NavLink className={`${css["navLink"]} ${globalCss["global-link"]}`}>Drinks</NavLink>
-                    <NavLink className={`${css["navLink"]} ${globalCss["global-link"]}`}>Add drink</NavLink>
-                    <NavLink className={`${css["navLink"]} ${globalCss["global-link"]}`}>My drinks</NavLink>
-                    <NavLink className={`${css["navLink"]} ${globalCss["global-link"]}`}>Favorites drinks</NavLink>
-                </nav>
-            </div>
-
-
-            <div className={css.subscribeBox}>
-                <p className={`${css["subscribeP"]} ${globalCss["global-p"]}`}>Subscribe up to our newsletter. Be in touch with latest news and special offers, etc.</p>
-
-                <form className={css.subscribeForm} action="">
-                    <label className={css.subscribeLabel} htmlFor="subscribe">
-                        <input 
-                        id="subscribe"
-                        className={css.subscribeInput}
-                        type="text"
-                        placeholder='Enter the email'
-                        />
-                    </label>
-                    <button className={css.subscribeBtn}>Subscribe</button>
-                </form>
+                <div className={css.box2}>
+                    <div className={css.subscribeBox}>
+                        <p className={`${css["subscribeP"]} ${globalCss["global-p"]}`}>Subscribe up to our newsletter. Be in touch with latest news and special offers, etc.</p>
+                        <form className={css.subscribeForm} action="">
+                            <label className={css.subscribeLabel} htmlFor="subscribe">
+                                <input
+                                id="subscribe"
+                                className={css.subscribeInput}
+                                type="text"
+                                placeholder='Enter the email'
+                                />
+                            </label>
+                            <button className={css.subscribeBtn}>Subscribe</button>
+                        </form>
+                    </div>
+                </div>
             </div>
 
 
             <div className={css.serviceBox}>
                 <p className={`${css["serviceP"]} ${globalCss["global-p"]}`}>©2023 Drink Master. All rights reserved.</p>
-                <a className={`${css["serviceLink"]} ${globalCss["global-link"]}`} href="#">Privacy Policy</a>
-                <a className={`${css["serviceLink"]} ${globalCss["global-link"]}`} href="#">Terms of Service</a>
+                <div className={css.serviceLinkBox}>
+                    <a className={`${css["serviceLink"]} ${globalCss["global-link"]}`} href="#">Privacy Policy</a>
+                    <a className={`${css["serviceLink"]} ${globalCss["global-link"]}`} href="#">Terms of Service</a>
+                </div>
             </div>
         </footer>
     )
