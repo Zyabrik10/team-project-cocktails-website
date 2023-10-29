d9d24a3d6183eb1075f39c5bbb07e2611536450d
Misha <shevchenkom606@gmail.com>
2023-10-29 16:17:51 +0100

    def path

--- src/layout/StartPages/StartPages.jsx
+++ src/layout/StartPages/StartPages.jsx
@@ -2,6 +2,8 @@ import { Outlet } from 'react-router-dom';
 
 import css from './StartPages.module.css';
 import globalCss from '../../css/global.module.css';
+import { Suspense } from 'react';
+
 
 export const StartPages = () => {
   return (
@@ -10,8 +12,10 @@ export const StartPages = () => {
         <div className={css['des-1']}></div>
         <div className={css['des-2']}></div>
         <div className={css['des-3']}></div>
-        <div style={{ position: 'relative', zIndex: '100', width:"100%" }}>
-          <Outlet />
+        <div style={{ position: 'relative', zIndex: '100', width: "100%" }}>
+          <Suspense>
+            <Outlet />
+          </Suspense>
         </div>
       </div>
     </div>
