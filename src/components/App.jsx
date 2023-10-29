1d4f1c6065130bdf82ba9cf605334ba4a2acb345
Olga16119 <tkachenko93@ukr.net>
2023-10-29 22:53:08 +0100

    update main

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,11 @@
-// import { lazy } from 'react';
-// import { useDispatch } from 'react-redux';
+import { StartPages } from 'layout/StartPages/StartPages';
 import { Routes, Route } from 'react-router-dom';
 import {SecMyDrinks} from '../pages/my-drinks/sec-my-drinks/SecMyDrinks'
 
-// import { PrivateRoute } from '../hooks/PrivateRoute';
-// import { RestrictedRoute } from '../hooks/RestrictedRoute';
-// import { refreshUser } from '../redux/auth/operations';
-// import { useAuth } from '../hooks';
-
-import { Layout } from '../layout/layout';
-import { Signin, Signup, Start } from 'pages';
-// import { StartPages } from 'layout/StartPages/StartPages';
-
-
-// const StartPage = lazy(() => import('../pages/start/Start'));
-// const SignupPage = lazy(() => import('../pages/signup/Signup'));
-// const SigninPage = lazy(() => import('../pages/signin/Signin'));
-// const HomePage = lazy(() => import('../pages/home/Home'));
-// const AddDrinkPage = lazy(() => import('../pages/add-drink/AddDrinks'));
-// const MyDrinksPage = lazy(() => import('../pages/my-drinks/MyDrinks'));
-// const FavoritesPage = lazy(() => import('../pages/favorites/Favorites'));
-// const DrinksPage = lazy(() => import('../pages/drinks/Drinks'));
-
-
-
-
-
+import Layout from '../layout/layout';
+import { Signin, Signup, Start } from '../pages/index';
 
 export const App = () => {
-  // const dispatch = useDispatch();
-  // const { isRefreshing } = useAuth();
   const isAuth = false
 
   return (
@@ -48,35 +24,12 @@ export const App = () => {
             <Route path='/home' element={<div><h1>HOME</h1></div>} />
             <Route path='/drinks' element={<div><h1>DRINKS</h1></div>} />
             <Route path='/add-drink' element={<div><h1>ADD-DRINK</h1></div>} />
-            <Route path='/my-drinks' element={<div><h1>MY-DRINKS</h1></div>} />
+            <Route path='/my-drinks' element={<SecMyDrinks/>} />
             <Route path='/favorites' element={<div><h1>FAVORITES</h1></div>} />
           </Routes>
+        </Layout>
       )}
     </>
 
   );
 };
-
-// Rabotaet
-      // <Routes>
-      //   <Route path='/' element={<Layout/>}>
-      //     <Route index  path="/home" element={<HomePage/>} />
-      //   </Route>
-      // </Routes>
-      // <Routes>
-      //   <Route path="/welcome" element={<StartPages />}>
-      //     <Route index element={ <StartPage/>} />
-      //     <Route
-      //       path="/welcome/signup"
-      //       element={
-      //         <RestrictedRoute redirectTo="/home" component={<SignupPage />} />
-      //       }
-      //     />
-      //     <Route
-      //       path="/welcome/signin"
-      //       element={
-      //         <RestrictedRoute redirectTo="/home" component={<SigninPage />} />
-      //       }
-      //     />
-      //   </Route>
-      // </Routes>
