b6cc5c850ba5ead850352b4c42555851b1f536fd
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-10-30 21:03:19 -0400

    Major CSS for Drink_ID completed for all screens

--- src/components/MyDrink/Preparation/Preparation.jsx
+++ src/components/MyDrink/Preparation/Preparation.jsx
@@ -14,24 +14,32 @@ export const Preparation = () => {
       >
         Recipe Preparation
       </h2>
-      <div className={`${css['description-section']}`}>
-        <p
-          className={`${globalCss['global-p']} ${css['preparation-description']}`}
-        >
-          {recipe.instructions}
-        </p>
-        <p
-          className={`${globalCss['global-p']} ${css['preparation-description']}`}
-        >
-          {recipe.instructionsDE}
-        </p>
-        <p
-          className={`${globalCss['global-p']} ${css['preparation-description']}`}
-        >
-          {recipe.instructionsES}
-        </p>
+      <div className={`${css['preparation-body']}`}>
+        <div className={`${css['description-section']}`}>
+          <p
+            className={`${globalCss['global-p']} ${css['preparation-description']}`}
+          >
+            {recipe.instructions}
+          </p>
+          <p
+            className={`${globalCss['global-p']} ${css['preparation-description']}`}
+          >
+            {recipe.instructionsDE}
+          </p>
+          <p
+            className={`${globalCss['global-p']} ${css['preparation-description']}`}
+          >
+            {recipe.instructionsES}
+          </p>
+        </div>
+        <div className={`img-preparation-section`}>
+          <img
+            className={`${css['img-preparation']} `}
+            src={recipe.drinkThumb}
+            alt="drink"
+          />
+        </div>
       </div>
-      <img src={recipe.drinkThumb} alt="drink" width={335} height={430} />
     </div>
   );
 };
