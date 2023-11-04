745be5768b16535c60965801b966e4dd7716663c
Alex Mazurok | Мазурок Олександр | Zyabrik10 <78109707+Zyabrik10@users.noreply.github.com>
2023-11-04 12:42:06 +0100

    Modal window press escape

--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -1,12 +1,13 @@
 import css from './ModalWindow.module.css';
 
-export const ModalWindow = ({ children, isShown, setClose }) => {
+export const ModalWindow = ({ children, isShown, setClose, closeOnKeyDown }) => {
   const closeOnClick = ({ target }) => {
     if (
       target.classList.value !== '' &&
       target.classList[0] === css['modal-wrapper']
     )
       setClose(false);
+    document.removeEventListener('keydown', closeOnKeyDown);
   };
 
   return (
