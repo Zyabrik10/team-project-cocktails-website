ed48cef05f8a03e6c8cfe99d7321a173870de316
Alex Mazurok | Мазурок Олександр | Zyabrik10 <78109707+Zyabrik10@users.noreply.github.com>
2023-11-04 13:52:34 +0100

    hero fix

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,17 @@
 import { DrinkContainer } from './components/DrinkContainer';
 import { Hero } from './components/sec-hero/Hero';
-import css from "./Home.module.css"
+import css from './Home.module.css';
 
 export default function Home() {
   return (
-      <div className={css.container}>
+    <div className={css.container}>
+      <div className={css['dec-1']}></div>
+      <div className={css['dec-2']}></div>
+      <div className={css['dec-3']}></div>
+      <div style={{ position: "reletive", zIndex: 10 }}>
         <Hero />
         <DrinkContainer />
       </div>
+    </div>
   );
 }
