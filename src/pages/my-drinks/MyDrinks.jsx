d46451590a7f351e9dbc795b33c830fdfc40912f
Olga16119 <tkachenko93@ukr.net>
2023-11-05 21:45:38 +0100

    add pageLayout

--- src/pages/my-drinks/MyDrinks.jsx
+++ src/pages/my-drinks/MyDrinks.jsx
@@ -1,13 +1,10 @@
-import css from './sec-my-drinks/SecMyDrinks.module.css';
+import PageLayout from '../../components/PageLayout/PageLayout';
 import { SecMyDrinks } from './sec-my-drinks/SecMyDrinks';
 
 export default function MyDrinks() {
   return (
-    <>
-      <div className={css.gradient1}></div>
-      <div className={css.gradient2}></div>
-      <div className={css.gradient3}></div>
+    <PageLayout>
       <SecMyDrinks />
-    </>
+    </PageLayout>
   );
 }
