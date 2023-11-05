4dd48e9e6d1d4100e34e0e6da50eb3cc1bc3c7d7
EdHloba <hloba.e.v@gmail.com>
2023-11-05 17:52:49 +0200

    for Dima

--- src/pages/drinks/sec-drinks/SecDrinks.jsx
+++ src/pages/drinks/sec-drinks/SecDrinks.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import Select from 'react-select';
-import './react-select.css';
+
 import { useState, useEffect } from 'react';
 import recipes from './recipes.json';
 
 import DrinkList from 'components/DrinksList/DrinksList';
 import { Title } from 'components/Title/Title';
 import Filter from 'components/DrinksList/Filter';
+import selectsStyles from 'components/DrinksList/styles/selectStyles';
+
+import css from './secDrinks.module.css';
+import SelectInput from 'components/SelectInput';
+import filtersAPI from 'redux/api/filtersAPI';
+import makeSelectOptions from 'components/AddDrinkForm/utils/makeSelectOptions';
 
 export default function SecDrinks() {
   const [cocktails, setCocktails] = useState([]);
   const [filter, setFilter] = useState('');
-  const [currentCountry, setCurrentCountry] = useState('');
+  // const [currentCountry, setCurrentCountry] = useState('');
+  const [ingredients, setIngredients] = useState('');
+  const [category, setCategory] = useState('');
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
@@ -27,32 +34,74 @@ export default function SecDrinks() {
     setFilter(value);
   };
 
-  const options = [
-    { value: 'argentina', label: 'Argentina' },
-    { value: 'german', label: 'German' },
-    { value: 'japan', label: 'Japan' },
-    { value: 'poland', label: 'Poland' },
-  ];
-  const getValue = () => {
-    return currentCountry
-      ? options.find(country => country.value === currentCountry)
-      : '';
+  // const options = [
+  //   { value: 'argentina', label: 'Argentina' },
+  //   { value: 'german', label: 'German' },
+  //   { value: 'japan', label: 'Japan' },
+  //   { value: 'poland', label: 'Poland' },
+  //   { value: 'argentina', label: 'Argentina' },
+  //   { value: 'german', label: 'German' },
+  //   { value: 'japan', label: 'Japan' },
+  //   { value: 'poland', label: 'Poland' },
+  //   { value: 'argentina', label: 'Argentina' },
+  //   { value: 'german', label: 'German' },
+  //   { value: 'japan', label: 'Japan' },
+  //   { value: 'poland', label: 'Poland' },
+  // ];
+  // const getValue = () => {
+  //   return currentCountry
+  //     ? options.find(country => country.value === currentCountry)
+  //     : '';
+  // };
+  // const onChange = newValue => {
+  //   setCurrentCountry(newValue.value);
+  // };
+  const handleSelectChange = selectData => {
+    const { name, value } = selectData;
+    console.log(value);
+    switch (name) {
+      case 'ingredients': {
+        setIngredients(value);
+        
+        return;
+      }
+      case 'category': {
+        setCategory(value);
+        return;
+      }
+      default:
+        return;
+    }
   };
-  const onChange = newValue => {
-    setCurrentCountry(newValue.value);
-  };
-
   return (
     <>
       <Title children="Drinks" />
-      <Filter filter={filter} onChange={handleFilterChange} />
-      <Select
-        classNamePrefix="custom-select"
-        onChange={onChange}
-        value={getValue()}
-        options={options}
-        placeholder="All ingredients"
-      />
+      <div className={css.filter}>
+        <Filter filter={filter} onChange={handleFilterChange} />
+        {/* <Select
+          onChange={onChange}
+          value={getValue()}
+          options={options}
+          placeholder="All categories"
+          styles={selectsStyles}
+        /> */}
+        <SelectInput
+          inputName={'ingredients'}
+          fetchSelectOpt={filtersAPI.useGetIngredientsQuery}
+          handleSelectChange={handleSelectChange}
+          makeOptArr={makeSelectOptions}
+          defaultValue={ingredients}
+          styles={selectsStyles}
+        />
+        <SelectInput
+          inputName={'category'}
+          fetchSelectOpt={filtersAPI.useGetCategoriesQuery}
+          handleSelectChange={handleSelectChange}
+          makeOptArr={makeSelectOptions}
+          defaultValue={category}
+          styles={selectsStyles}
+        />
+      </div>
       <DrinkList cocktails={cocktails} />
     </>
   );
