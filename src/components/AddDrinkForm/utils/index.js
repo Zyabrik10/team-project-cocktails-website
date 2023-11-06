aa184ce1602272d62110d333e7b5cedac678b5f3
Dmytro Stas <dima7stas777@gmail.com>
2023-11-05 22:54:26 -0500

    Роботу над логікою модалки виконано

--- src/components/AddDrinkForm/utils/index.js
+++ src/components/AddDrinkForm/utils/index.js
@@ -2,3 +2,4 @@ export { default as validateIngredients } from './validateIngredients';
 export { default as validationSchema } from './validationSchema';
 export { default as getSelectOptions } from './makeSelectOptions';
 export { default as makeIngrSelectOptions } from './makeIngrSelectOptions';
+export { default as makeFormData } from './makeFormData';
--- src/components/ModalWindow/components/Svg/AddSvg.jsx
+++ src/components/ModalWindow/components/Svg/AddSvg.jsx
@@ -1,13 +1,39 @@
 import css from '../../components/UserProfile/UserProfile.module.css';
 
-
 export const AddSvg = () => {
-    return (
-        <svg  className={css.addSvg}  viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="14" cy="14" r="14" fill="#546081"/>
-            <path d="M14.2188 9.625V18.8125" stroke="#F3F3F3" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-            <path d="M9.625 14.2188H18.8125" stroke="#F3F3F3" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-        </svg>
-    );
-  }
-  export default AddSvg;
+  return (
+    <svg
+      className={css.addSvg}
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+    >
+      <title>add-photo</title>
+      <path
+        fill="currentColor"
+        d="M16 8c0 4.418-3.582 8-8 8s-8-3.582-8-8c0-4.418 3.582-8 8-8s8 3.582 8 8z"
+      ></path>
+      <path
+        className={css.icon}
+        fill="none"
+        strokeLinejoin="round"
+        strokeLinecap="round"
+        strokeMiterlimit="4"
+        strokeWidth="1"
+        d="M8.125 5.5v5.25"
+      ></path>
+      <path
+        className={css.icon}
+        fill="none"
+        strokeLinejoin="round"
+        strokeLinecap="round"
+        strokeMiterlimit="4"
+        strokeWidth="1"
+        d="M5.5 8.125h5.25"
+      ></path>
+    </svg>
+  );
+};
+export default AddSvg;
